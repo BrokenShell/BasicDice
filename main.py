cc50882867badea650ce03a17537a26315ddacc6
BrokenShell <webmaster@sharpdesigndigital.com>
2022-11-17 15:56:01 -0800

    adds docstrings

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import random
 
 
 def d(sides: int) -> int:
+    """ Rolls one die of a given size, returns the value rolled """
     return random.randint(1, sides)
 
 
 def dice(rolls: int, sides: int) -> int:
+    """ Rolls multiple dice of a given size, returns the sum total """
     return sum(d(sides) for _ in range(rolls))
 
 
